161f02884a078c3185adfea2e9d9cc157a02a81f
Daniel Sank <sank.daniel@gmail.com>
2016-12-09 17:54:48 -0800

    Convert to python in IQ demodulated noise.

---
+++ SignalProcessing/IQ-demodulated-noise/make-figures.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+filenames = [
+    'IQ_clouds',
+    'signal_processing_chain'
+]
+
+
+command = ''
+for name in filenames:
+    command += "echo {}.svg --export-pdf={}.pdf;\n".format(name, name)
+command = "({}) |\nDISPLAY= inkscape --shell".format(command)
+
+
+import os
+os.system(command)
+
--- SignalProcessing/IQ-demodulated-noise/make-figures.sh
+++
@@ -1,4 +0,0 @@
-(echo ./IQ_clouds.svg --export-pdf=./IQ_clouds.pdf;
- echo ./signal_processing_chain.svg --export-pdf=./signal_processing_chain.pdf) |
-  DISPLAY= inkscape --shell
-
