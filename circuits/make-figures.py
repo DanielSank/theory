84df52e9b1e9d975747ef795a72af518513db520
Daniel Sank <sank.daniel@gmail.com>
2017-02-09 10:37:04 -0800

    Fix long line.

--- circuits/make-figures.py
+++ circuits/make-figures.py
@@ -10,7 +10,8 @@ import os
 
 command = ''
 for name in filenames:
-    command += "echo {}/{}.svg --export-pdf={}/{}.pdf;\n".format(os.getcwd(),name, os.getcwd(),name)
+    command += "echo {}/{}.svg --export-pdf={}/{}.pdf;\n".format(
+            os.getcwd(),name, os.getcwd(),name)
 command = "({}) |\nDISPLAY= inkscape --shell".format(command)
 
 
