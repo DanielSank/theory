64a12ed5d40daf5c7a00d908947f2aa69c3bcab5
Daniel Sank <sank.daniel@gmail.com>
2017-02-09 10:37:49 -0800

    Fix spacing

--- circuits/make-figures.py
+++ circuits/make-figures.py
@@ -11,7 +11,7 @@ import os
 command = ''
 for name in filenames:
     command += "echo {}/{}.svg --export-pdf={}/{}.pdf;\n".format(
-            os.getcwd(),name, os.getcwd(),name)
+            os.getcwd(), name, os.getcwd(), name)
 command = "({}) |\nDISPLAY= inkscape --shell".format(command)
 
 
