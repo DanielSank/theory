5df3d9256a3de310b0dd9a3bb2c3b4a7a53c9b61
Daniel Sank <sank.daniel@gmail.com>
2017-11-09 09:44:00 -0800

    Fix filenames in derivation-1.

---
+++ DispersiveReadout/purcell/derivation-1/make_figures.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+filenames = [
+    'figure1',
+    'figure2',
+    'figure3',
+]
+
+import os
+
+command = ''
+for name in filenames:
+    command += "echo {}/{}.svg --export-pdf={}/{}.pdf;\n".format(
+            os.getcwd(), name, os.getcwd(), name)
+command = "({}) |\nDISPLAY= inkscape --shell".format(command)
+
+
+os.system(command)
+
