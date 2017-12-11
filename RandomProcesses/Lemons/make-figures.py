77cead4daca3a158cba1f3a6f51895827c56dd66
Daniel Sank <sank.daniel@gmail.com>
2017-12-10 19:14:35 -0800

    Add some chapter 4 problems.

---
+++ RandomProcesses/Lemons/make-figures.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+filenames = [
+    '4.1_maps'
+]
+
+
+command = ''
+for name in filenames:
+    command += "echo {}.svg --export-pdf={}.pdf;\n".format(name, name)
+command = "({}) |\nDISPLAY= inkscape --shell".format(command)
+
+
+import os
+os.system(command)
+
