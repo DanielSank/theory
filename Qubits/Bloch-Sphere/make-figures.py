00d954116ca7a50e2656b15dbeccac8afa8f0e25
Daniel Sank <sank.daniel@gmail.com>
2020-01-26 15:31:33 -0800

    Got it building

---
+++ Qubits/Bloch-Sphere/make-figures.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+filenames = [
+    'bloch_sphere',
+    'iq_trajectories',
+]
+
+
+command = ''
+for name in filenames:
+    command += "echo {}.svg --export-pdf={}.pdf;\n".format(name, name)
+command = "({}) |\nDISPLAY= inkscape --shell".format(command)
+
+
+import os
+os.system(command)
+
