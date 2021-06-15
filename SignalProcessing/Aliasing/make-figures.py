047b8afc44ca889ed11af16386d4ca3bd53d7c16
Daniel Sank <sank.daniel@gmail.com>
2021-06-14 20:13:26 -0700

    Fix figure-maker in Aliasing writeup.

--- SignalProcessing/Aliasing/make-figures.py
+++ SignalProcessing/Aliasing/make-figures.py
@@ -1,4 +1,6 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+
+import os
 
 filenames = [
     'aliasing_real',
@@ -9,12 +11,5 @@ filenames = [
 ]
 
 
-command = ''
 for name in filenames:
-    command += "echo {}.svg --export-pdf={}.pdf;\n".format(name, name)
-command = "({}) |\nDISPLAY= inkscape --shell".format(command)
-
-
-import os
-os.system(command)
-
+    os.system(f'inkscape --export-type="pdf" {name}.svg')
